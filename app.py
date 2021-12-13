d37a8c6c76cdea5c310132fda6381d00deea7c36
liying <LiYing11@protonmail.com>
2021-12-13 19:31:01 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, escape, url_for
+# -*- coding: utf-8-*-
+from flask import Flask, escape, url_for, render_template
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
-def hello():
-    return '<h1>Hello Totoro!</h1><h2>Welcome to my watchlist!</h2><img src="http://helloflask.com/totoro.gif">'
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -22,3 +23,18 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+
+
+name = 'LiYing'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
